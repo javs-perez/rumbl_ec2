6b6d9088cdaaa1645430ea250c601a3d8f8b040e
Javier Perez <perez.javi.a@gmail.com>
2016-10-18 11:20:54 -0400

    added user view and controller action for show

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -5,4 +5,9 @@ defmodule RumblEc2.UserController do
     users = Repo.all(RumblEc2.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(RumblEc2.User, id)
+    render conn, "show.html", user: user
+  end
 end
