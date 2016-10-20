9da828646a07058cba399ed91103f0faee15602d
Javier Perez <perez.javi.a@gmail.com>
2016-10-20 12:10:18 -0400

    fixed a test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RumblEc2.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix runninng in a EC2 instance."
   end
 end
