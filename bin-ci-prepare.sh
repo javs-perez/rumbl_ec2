dce1b433e5a477a7782b36e00613d5692b0c1b11
Javier Perez <perez.javi.a@gmail.com>
2016-11-01 15:03:38 -0400

    install in prepare.sh

--- bin-ci-prepare.sh
+++ bin-ci-prepare.sh
@@ -57,6 +57,9 @@ if [ ! -e $HOME/.mix/rebar ]; then
   yes Y | LC_ALL=en_GB.UTF-8 mix local.rebar
 fi
 
+apt-get install build-essential -y
+apt-get install erlang-dev -y
+
 # Fetch and compile dependencies and application code (and include testing tools)
 export MIX_ENV="test"
 cd $HOME/$CIRCLE_PROJECT_REPONAME
